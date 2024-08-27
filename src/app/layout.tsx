d0b92e709c12740d1c4d745376c1304ea86a910b
tasnimzotder <hello@tasnim.dev>
2024-08-28 01:21:23 +0530

    fix font (nanummyeongjo)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,21 +12,6 @@ import Footer from "@/components/footer/footer";
 import ThemeProvider from "@/components/providers/theme_provider";
 
 const inter = Inter({ subsets: ["latin"] });
-const nanumMyeongjo = Nanum_Myeongjo({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  fallback: ["inter", "sans-serif"],
-});
-const sourceSerif = Source_Serif_4({
-  subsets: ["latin"],
-  weight: ["300", "400", "600", "700"],
-  preload: true,
-});
-const notoSerif = Noto_Serif({
-  subsets: ["latin"],
-  weight: ["200", "300", "400", "700"],
-  preload: true,
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
